54c120e6f90d285d373743e28b8cb32e720d66d3
kancharlaajay <kancharlaajay1234@gmail.com>
2025-10-25 12:33:42 +0530

    installed eksctl and kubectl

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "this" {
   ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [aws_security_group.allow_all_docker.id]
+  subnet_id              = "subnet-09bbafbc25928a024"
   instance_type          = "t3.micro"
+  associate_public_ip_address = true
 
   # 20GB is not enough
   root_block_device {
